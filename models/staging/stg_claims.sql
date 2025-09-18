cb7bcffcc52b47a1f8c9475e92c3c19c20584b3a
Colin Thornburg <colin.thornburg@fishtownanalytics.com>
2025-09-18 12:07:01 +0000

    mods

--- models/staging/stg_claims.sql
+++ models/staging/stg_claims.sql
@@ -12,7 +12,7 @@ SELECT
     member_responsibility::DECIMAL(10,2) as member_responsibility,
     claim_status,
     service_date::DATE as service_date,
-    YEAR(claim_date) as claim_year,
-    MONTH(claim_date) as claim_month,
-    CURRENT_TIMESTAMP() as loaded_at
+    EXTRACT(YEAR FROM claim_date) as claim_year,
+    EXTRACT(MONTH FROM claim_date) as claim_month,
+    CURRENT_TIMESTAMP as loaded_at
 FROM {{ ref('claims') }}
